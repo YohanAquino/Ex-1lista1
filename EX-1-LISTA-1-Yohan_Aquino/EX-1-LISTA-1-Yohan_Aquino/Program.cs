a1bfdae428f1f293ed92494d5d5a6166fbd7cd70
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:26:52 -0300

    Add files via upload

---
+++ EX-1-LISTA-1-Yohan_Aquino/EX-1-LISTA-1-Yohan_Aquino/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_1_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Entrar via teclado com a base e a altura de um retângulo, calcular e exibir sua área.
+
+            Retangulo ret = new Retangulo();
+
+            Console.Write("Informe a base do retângulo: ");
+            ret.setBase(double.Parse(Console.ReadLine()));
+
+            Console.Write("Informe a altura do retângulo: ");
+            ret.setAltura(double.Parse(Console.ReadLine()));
+
+            ret.calcularArea();
+
+            Console.WriteLine("O retângulo tem área igual a {0} ", ret.getArea());
+            Console.ReadKey();
+        }
+
+    }
+    }
+
---
+++ EX-1-LISTA-1-Yohan_Aquino/EX-1-LISTA-1-Yohan_Aquino/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_1_LISTA_1_Yohan_Aquino
+{
+    class Retangulo
+    {
+        private double b;
+        private double h;
+        private double a;
+
+        public void setBase(double x)
+        {
+            b = x;
+        }
+
+        public void setAltura(double x)
+        {
+            h = x;
+        }
+        public void calcularArea()
+        {
+            a = b * h;
+        }
+        public double getArea()
+        {
+            return a;
+        }
+
+    }
+}
